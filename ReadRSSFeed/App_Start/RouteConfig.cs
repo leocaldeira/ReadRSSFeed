cd0c00b6822e846e849e0456738f9b23efc45041
Leonardo <lilo.caldeira@hotmail.com>
2018-10-15 17:37:29 -0300

    Carousel e demais notícias adicionadas

--- ReadRSSFeed/App_Start/RouteConfig.cs
+++ ReadRSSFeed/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ReadRSSFeed
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Feed", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
